7edbaf9e3a40d1ebab7d99ae1ce27a591df2b47b
ebruakbulut <41595775+ebruakbulut@users.noreply.github.com>
2021-08-15 15:25:35 +0300

    Create SQL_odev3.sql

---
+++ SQL_odev3.sql
@@ -0,0 +1,4 @@
+select country from country where country LIKE 'A%a';
+select * from country where country like '______%n';
+select * from film where title ilike '%t%t%t%t%' ;
+select * from film where title like 'C%' and length>90 and rental_rate=2.99;
